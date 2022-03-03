c31e8a596629e7f72152b9ac030b62eb0d750049
Chandan <chandancvp08@gmail.com>
2022-03-03 21:52:54 +0530

    done with most of the stuffgit add .

--- components.py
+++ components.py
@@ -24,12 +24,16 @@ def mineTransactions(user,updateComponent):
     root= tk.Tk()
     root.geometry("500x500")
     tk.Label(root,text="We love you for maintaining the network!!").pack()
-    tk.Label(root,text="mining!!").pack()
-    TaaCoin.minePendingTransactions(user.id)
-    tk.Label(root,text="done mining!!").pack()
-    user.balance = TaaCoin.getBalanceof(user.id)
-    tk.Label(root,text=("as a token of appreciation we added"+str(TaaCoin.miningReward)+"to your account")).pack()
-    print(user.balance)
-    updateComponent.config(text=("Balance:"+str(user.balance)))
+    if(len(TaaCoin.peningTransactions)<=3):
+        tk.Label(root, text= "please wait till pending transanction count reaches to 3+").pack()
+        tk.Label(root, text="current pending transanctions: "+ str(len(TaaCoin.peningTransactions))).pack()
+    else:
+        tk.Label(root,text="mining!!").pack()
+        TaaCoin.minePendingTransactions(user.id)
+        tk.Label(root,text="done mining!!").pack()
+        user.balance = TaaCoin.getBalanceof(user.id)
+        tk.Label(root,text=("as a token of appreciation we added"+str(TaaCoin.miningReward)+"to your account")).pack()
+        print(user.balance)
+        updateComponent.config(text=("Balance:"+str(user.balance)))
     root.mainloop()
     
